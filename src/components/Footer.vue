6633a26e116786103d36e42d7441b06bd917a18a
iram96 <mariano.maselli@virgilio.it>
2022-02-22 03:04:26 +0100

    Added base Structure of Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,32 @@
 <template>
-  <div></div>
+  <div id="footer">
+    <section id="email-submit">
+      <div class="container-app"></div>
+    </section>
+    <section id="shopping-info"></section>
+    <section id="end-page"></section>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Footer",
+};
 </script>
 
-<style>
+<style scoped lang='scss'>
+#footer {
+  #email-submit {
+    background-color: #2e3238;
+    min-height: 150px;
+  }
+  #shopping-info {
+    background-color: #2b2e32;
+    min-height: 300px;
+  }
+  #end-page {
+    background-color: #1c1e21;
+    min-height: 100px;
+  }
+}
 </style>
